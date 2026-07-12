bd39f13fcab13b0e1c55fb6bef730cfa30510104
normansmart <normansmart14@gmail.com>
2026-07-11 23:42:33 -0400

    First Commit

---
+++ src/pages/Homepage.jsx
@@ -0,0 +1,14 @@
+
+
+
+function Homepage() {
+  return (
+    <div className="Homepage">
+      <header className="Homepage-header">
+        <h1>Blossom & Boom</h1>
+      </header>
+    </div>
+  )
+}
+
+export default Homepage
