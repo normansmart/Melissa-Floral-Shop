3faf5ac9cdcae64fd0dee7e160a0f53d0224484b
RustySyrup <alexisgagnon81@gmail.com>
2026-07-12 19:52:49 -0700

    added font family and added homepage css
    some of this css will be recreated at later date due to them wanting something to see for next week, also as soon as we create buttons we can further the viewing of website

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,44 +1,68 @@
+import "./Homepage.css";
+
 function Homepage() {
   return (
     <div className="Homepage">
       {/*HEADER*/}
       <header className="Homepage-header">
-        <h1>Flores Con Fuego.</h1>
-        <p>
-          Luxury Floral Design rooted in Peruvian soul, blooming in the desert
-          of Las Vegas.
-        </p>
+        <div className="header-container">
+          <div className="h1-container">
+            <h1>Flores Con Fuego.</h1>
+            <p>
+              Luxury Floral Design rooted in Peruvian soul, blooming in the
+              desert of Las Vegas.
+            </p>
+          </div>
+          <div className="image-container"></div>
+        </div>
       </header>
       {/*HERO*/}
       <section>
-        <h2>From Peru to the desert.</h2>
-        <p>
-          Born in the heart of Peru, now designing in the heart of Las Vegas.
-        </p>
-        <p>
-          Blending Andean blooms with bold, modern elegance, I bring passion and
-          artistry to the desert’s most exquisite events.
-        </p>
-        <blockquote>
-          &quot;Flowers should feel like poetry, not decoration.&quot;
-        </blockquote>
+        <div className="section-container">
+          <div className="image-container"></div>
+          <div className="hero-container">
+            <h2>From Peru to the desert.</h2>
+            <p>
+              Born in the heart of Peru, now designing in the heart of Las
+              Vegas.
+            </p>
+            <p>
+              Blending Andean blooms with bold, modern elegance, I bring passion
+              and artistry to the desert’s most exquisite events.
+            </p>
+            <blockquote>
+              &quot;Flowers should feel like poetry, not decoration.&quot;
+            </blockquote>
+          </div>
+        </div>
         <h3>Signature Creations</h3>
       </section>
       {/*CARDS*/}
-      <section>
+      <section className="card-section">
         <h2>Rooted in Peruvian Heritage</h2>
         <p>
           inspired by Andean traditions, vibrant textiles, and the spirit of
           Andes
         </p>
-        <h3>Luxury Weddings</h3>
-        <p>Full-scale floral storytelling</p>
-        <h3>High- End Events</h3>
-        <p>Corporate & Celebrity installations</p>
-        <h3>Bespoke Bouquets</h3>
-        <p>Custom designed arrangements</p>
-        <h3>Floral Styling & Creative Direction</h3>
-        <p> For photoshoots and brands</p>
+        <div className="card-container">
+          <div className="card">
+            <h3>Luxury Weddings</h3>
+            <p>Full-scale floral storytelling</p>
+          </div>
+          <div className="card">
+            <h3>High- End Events</h3>
+            <p>Corporate & Celebrity installations</p>
+          </div>
+          <div className="card">
+            <h3>Bespoke Bouquets</h3>
+            <p>Custom designed arrangements</p>
+          </div>
+          <div className="card">
+            <h3>Floral Styling & Creative Direction</h3>
+            <p> For photoshoots and brands</p>
+          </div>
+        </div>
+
         <blockquote>
           &quot;Her Arrangements transformed our wedding into pure art.&quot;
           <span>John and Sarah</span>
